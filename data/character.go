4da15775bfa5d7c5a3b34461ec8352d6953979c4
hide168 <purplehaze19900918@gmail.com>
2020-08-15 20:50:07 +0900

    エラーの検証

--- data/character.go
+++ data/character.go
@@ -62,8 +62,8 @@ func CharacterByUUID(uuid string) (character Character, err error) {
 }
 
 func Search(word string) (characters []Character, err error) {
-	rows, err := Db.Query("SELECT id, uuid, name, text, user_id, image, created_at FROM characters WHERE name LIKE ?", fmt.Sprintf("'%%%s%%'", word))
-	test := fmt.Sprintf("'%%%s%%'", word)
+	rows, err := Db.Query("SELECT id, uuid, name, text, user_id, image, created_at FROM characters WHERE name LIKE ?", fmt.Sprintf("%%%s%%", word))
+	test := fmt.Sprintf("%%%s%%", word)
 	log.Print(test) //動作テスト
 	if err != nil {
 		return

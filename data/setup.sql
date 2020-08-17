8e892f0ce769fab76ae1d7de8029a1e87824d21e
hide168 <purplehaze19900918@gmail.com>
2020-08-17 16:52:42 +0900

    エラーを修正

--- data/setup.sql
+++ data/setup.sql
@@ -5,7 +5,7 @@ create table users (
     id          serial primary key,
     uuid        varchar(64) not null unique,
     name        varchar(255),
-    email       varchar(255) not null unique,
+    email       varchar(255) not null,
     password    varchar(255) not null,
     icon        varchar(255) not null,
     created_at  timestamp not null

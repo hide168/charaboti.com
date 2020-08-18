cb05d6b1e5365fa11c5151ab04c654329a5bfe8d
hide168 <purplehaze19900918@gmail.com>
2020-08-18 10:08:04 +0900

    エラーを修正

--- data/data_test.go
+++ data/data_test.go
@@ -2,27 +2,27 @@ package data
 
 var users = []User{
 	{
-		Name:     		"田中 太郎",
-		Email:    		"tarou@gmail.com",
-		Password: 		"taroupass",
+		Name:     "田中 太郎",
+		Email:    "tarou@gmail.com",
+		Password: "taroupass",
 	},
 	{
-		Name:     		"John Smith",
-		Email:    		"john@gmail.com",
-		Password: 		"johnpass",
+		Name:     "John Smith",
+		Email:    "john@gmail.com",
+		Password: "johnpass",
 	},
 }
 
 var characters = []Character{
 	{
-		Name: "テストくん"
-		Text: "テストキャラクター１号です"
-		Image: "/characters/default.jpg"
+		Name:  "テストくん",
+		Text:  "テストキャラクター１号です",
+		Image: "/characters/default.jpg",
 	},
 	{
-		Name: ""
-		Text: "テストキャラクター２号です"
-		Image: "/characters/default.jpg"
+		Name:  "",
+		Text:  "テストキャラクター２号です",
+		Image: "/characters/default.jpg",
 	},
 }
 
